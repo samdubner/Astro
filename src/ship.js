5c7124d065f098a7ca938a4d1687d89d3766e7ab
IsoVoyd <dubnersamuel@gmail.com>
2021-03-03 16:15:16 -0800

    have event handler deal with multiple keypresses at once

--- src/ship.js
+++ src/ship.js
@@ -3,43 +3,30 @@ import Entity from "./entity";
 class Ship extends Entity {
   constructor(ctx) {
     super(ctx);
-    this.moveSpeed = 2;
-    this.rotationSpeed = 5;
+    this.moveSpeed = 1;
+    this.rotationSpeed = 2;
 
     this.pos = [500, 500];
     this.rotation = 0;
 
-    this.keypressHandler = this.keypressHandler.bind(this);
-    document.addEventListener("keydown", this.keypressHandler);
-  }
+    this.keypresses = {
+      ArrowUp: { pressed: false, func: () => this.updateVelocity("forward") },
+      ArrowDown: { pressed: false, func: () => this.updateVelocity("backward") },
+      ArrowRight: { presssed: false, func: () => this.updateRotation("right") },
+      ArrowLeft: { pressed: false, func: () => this.updateRotation("left") },
+    };
 
-  keypressHandler(e) {
-    switch (e.key) {
-      case "w":
-      case "ArrowUp":
-        this.updateVelocity("forward");
-        break;
-      case "s":
-      case "ArrowDown":
-        this.updateVelocity("backward");
-        break;
-      case "d":
-      case "ArrowRight":
-        this.updateRotation("right");
-        break;
-      case "a":
-      case "ArrowLeft":
-        this.updateRotation("left");
-        break;
-      case " ":
-        console.log("shoot");
-        break;
-      default:
-        console.log(e.key);
-    }
+    document.addEventListener("keydown", (e) => {
+      if (e.key in this.keypresses) this.keypresses[e.key].pressed = true;
+    });
+
+    document.addEventListener("keyup", (e) => {
+      if (e.key in this.keypresses) this.keypresses[e.key].pressed = false;
+    });
   }
 
   updateVelocity(direction) {
+    if (Math.sqrt((this.vel[0] ** 2) + (this.vel[1] ** 2)) > 8) return;
     switch (direction) {
       case "forward":
         this.vel[0] +=
@@ -81,36 +68,48 @@ class Ship extends Entity {
     posTextY.innerHTML = `y: ${y}`;
   }
 
+  executeKeydowns() {
+      for (let key in this.keypresses) {
+          if (this.keypresses[key].pressed) this.keypresses[key].func();
+      }
+  }
+
   draw() {
+    this.executeKeydowns();
     this.updateUi();
-    if (Math.abs(this.vel[0]) - 0.1 < 0.1) this.vel[0] = 0;
-    if (Math.abs(this.vel[1]) - 0.1 < 0.1) this.vel[1] = 0;
+    //wrap spaceship
+    if (this.pos[0] < 0) this.pos[0] += window.innerWidth;
+    if (this.pos[1] < 0) this.pos[1] += window.innerHeight;
     this.pos[0] = this.pos[0] % window.innerWidth;
     this.pos[1] = this.pos[1] % window.innerHeight;
-        if (this.pos[0] < 0)
-          this.pos[0] = window.innerWidth;
-        if (this.pos[1] < 0)
-          this.pos[1] += window.innerHeight;
 
+    //Have ship stop moving in case space drag didn't entirely stop it
+    if (Math.abs(this.vel[0]) - 0.1 < 0.1) this.vel[0] = 0;
+    if (Math.abs(this.vel[1]) - 0.1 < 0.1) this.vel[1] = 0;
+
+    //space drag
     if (this.vel[0] > 0) {
       this.vel[0] -= 0.1;
     } else if (this.vel[0] < 0) {
       this.vel[0] += 0.1;
     }
 
+    //space drag pt.2
     if (this.vel[1] > 0) {
       this.vel[1] -= 0.1;
     } else if (this.vel[1] < 0) {
       this.vel[1] += 0.1;
     }
 
-    this.pos[0] += this.vel[0]
-    this.pos[1] += this.vel[1]
+    //update position
+    this.pos[0] += this.vel[0];
+    this.pos[1] += this.vel[1];
 
+    //draw ship
     this.ctx.save();
     this.ctx.beginPath();
     this.ctx.translate(this.pos[0], this.pos[1]);
-    this.ctx.rotate((Math.PI / 180) * (this.rotation - 90) );
+    this.ctx.rotate((Math.PI / 180) * (this.rotation - 90));
     this.ctx.moveTo(-25, 0);
     this.ctx.lineTo(0, 50);
     this.ctx.lineTo(25, 0);
