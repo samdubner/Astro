2ea343d17f6e91c273e919ff13aa028b812247f1
IsoVoyd <dubnersamuel@gmail.com>
2021-03-05 10:21:14 -0800

    have game start from main menu

--- src/index.js
+++ src/index.js
@@ -8,4 +8,19 @@ document.addEventListener("DOMContentLoaded", () => {
   ctx.canvas.height = window.innerHeight;
 
   let gameView = new GameView(ctx);
+
+  let mainMenu = document.getElementById("main-menu")
+  let uiOverlay = document.getElementById("ui-container")
+
+  let startButton = document.getElementById("start-game")
+  let htpButton = document.getElementById("instructions")
+  let aboutButton = document.getElementById("about-game")
+
+  startButton.addEventListener("click", (e) => {
+    mainMenu.style.visibility = "hidden"
+    uiOverlay.style.visibility = "visible"
+    canvas.style.visibility = "visible"
+    console.log("clicked start")
+    gameView.start();
+  })
 });
